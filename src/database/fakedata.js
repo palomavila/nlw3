b8d26a2cef82a550056bd3fe8a6c604088459e7f
Paloma V <62178655+palomavila@users.noreply.github.com>
2020-10-16 10:24:47 -0300

    Update fakedata.js

--- src/database/fakedata.js
+++ src/database/fakedata.js
@@ -1,30 +1,34 @@
 module.exports = [
-    {
-        id: 1,
-        lat: "-27.2114974",
-        lng: "-49.6444793",
-        name: "Lar das meninas",
-        description: "Presta assistência a crianças de 06 a 15 anos que se encontre em situação de risco e/ou vulnerabilidade social",
-        images: [
-            "https://images.unsplash.com/photo-1563465814437-b1a057a461ed?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjF9",
-            "https://images.unsplash.com/photo-1594575111057-47b35c5f98f7?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjF9",
-        ],
-        instructions: "Venha como se sentir a vontade e traga muito amor e paciência para dar",
-        opening_hours: "Horário de visitas Das 18h até 8h",
-        open_on_weekends: "1"
-    },
-    {
-        id: 2,
-        lat: "-27.2109325",
-        lng: "-49.6448719",
-        name: "Lar dos meninos",
-        description: "Presta assistência a crianças de 06 a 15 anos que se encontre em situação de risco e/ou vulnerabilidade social",
-        images: [
-            "https://images.unsplash.com/photo-1563465814437-b1a057a461ed?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjF9",
-            "https://images.unsplash.com/photo-1594575111057-47b35c5f98f7?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjF9",
-        ],
-        instructions: "Venha como se sentir a vontade e traga muito amor e paciência para dar",
-        opening_hours: "Horário de visitas Das 18h até 8h",
-        open_on_weekends: "0"
-    },
-]
+  {
+    id: 1,
+    lat: "-27.2114974",
+    lng: "-49.6444793",
+    name: "Lar das meninas",
+    description:
+      "Presta assistência a crianças de 06 a 15 anos que se encontre em situação de risco e/ou vulnerabilidade social",
+    images: [
+      "https://images.unsplash.com/photo-1583608205776-bfd35f0d9f83?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
+      "https://images.unsplash.com/photo-1493809842364-78817add7ffb?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
+    ],
+    instructions:
+      "Venha como se sentir a vontade e traga muito amor e paciência para dar",
+    opening_hours: "Horário de visitas Das 18h até 8h",
+    open_on_weekends: "1",
+  },
+  {
+    id: 2,
+    lat: "-27.2109325",
+    lng: "-49.6448719",
+    name: "Lar dos meninos",
+    description:
+      "Presta assistência a crianças de 06 a 15 anos que se encontre em situação de risco e/ou vulnerabilidade social",
+    images: [
+      "https://images.unsplash.com/photo-1502672260266-1c1ef2d93688?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
+      "https://images.unsplash.com/photo-1449844908441-8829872d2607?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
+    ],
+    instructions:
+      "Venha como se sentir a vontade e traga muito amor e paciência para dar",
+    opening_hours: "Horário de visitas Das 18h até 8h",
+    open_on_weekends: "0",
+  },
+];

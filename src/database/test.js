2b2389a22b71670f257047bfc9914bdaeeffc6e9
Paloma V <62178655+palomavila@users.noreply.github.com>
2020-10-16 10:29:21 -0300

    Create test.js

---
+++ src/database/test.js
@@ -0,0 +1,29 @@
+const Database = require("./db");
+const saveOrphanage = require("./saveOrphanage");
+
+Database.then(async (db) => {
+  await saveOrphanage(db, {
+    lat: "-27.2114974",
+    lng: "-49.6544793",
+    name: "Lar dos meninos",
+    about:
+      "Presta assistência a crianças de 06 a 15 anos que se encontre em situação de risco e/ou vulnerabilidade social",
+    whatsapp: "55999992222",
+    images: [
+      "https://images.unsplash.com/photo-1583608205776-bfd35f0d9f83?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
+      "https://images.unsplash.com/photo-1493809842364-78817add7ffb?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
+    ].toString(),
+    instructions:
+      "Venha como se sentir a vontade e traga muito amor e paciência para dar",
+    opening_hours: "Horário de visitas Das 18h até 8h",
+    open_on_weekends: "0",
+  });
+
+  const selectedOrphanages = await db.all("SELECT * FROM orphanages");
+  console.log(selectedOrphanages);
+
+  const orphanages = await db.all('SELECT * FROM orphanages WHERE id = "1"');
+  console.log(orphanages);
+
+  // console.log(await db.run("DELETE FROM orphanages WHERE id = '2'"))
+});

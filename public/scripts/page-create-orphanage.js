bf758b5a7ffb19d77325d8aaf97603940a3e4062
Paloma V <62178655+palomavila@users.noreply.github.com>
2020-10-17 00:46:05 -0300

    Update page-create-orphanage.js

--- public/scripts/page-create-orphanage.js
+++ public/scripts/page-create-orphanage.js
@@ -1,72 +1,84 @@
-const map = L.map("mapid").setView([-27.2109325, -49.6448719], 15);
+// create map
+const map = L.map('mapid').setView([-22.5076514,-43.1779323], 15);
 
-L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png").addTo(map);
+//create tileLayer
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(map);
 
+// create icon
 const icon = L.icon({
-  iconUrl: "/images/map-marker.svg",
-  iconSize: [58, 68],
-  iconAnchor: [29, 68],
+    iconUrl: "/images/map-marker.svg",
+    iconSize: [58,68],
+    iconAnchor: [29,68]
 });
 
+//create
 let marker;
+map.on('click', (event) => {
+    const lat = event.latlng.lat;
+    const lng = event.latlng.lng;
 
-map.on("click", (event) => {
-  const lat = event.latlng.lat;
-  const lng = event.latlng.lng;
+    document.querySelector('[name=lat]').value = lat;
+    document.querySelector('[name=lng]').value = lng;
+    // remove icon
+    marker && map.removeLayer(marker);
 
-  document.querySelector("[name=lat]").value = lat;
-  document.querySelector("[name=lng]").value = lng;
+    //add icon
+    marker = L.marker([lat,lng], { icon })
+    .addTo(map);
+})
 
-  marker && map.removeLayer(marker);
-
-  marker = L.marker([lat, lng], { icon }).addTo(map);
-});
-
-function addPhotoField() {
-  const container = document.querySelector("#images");
-
-  const fieldsContainer = document.querySelectorAll(".new-upload");
-
-  const newFieldContainer = fieldsContainer[
-    fieldsContainer.length - 1
-  ].cloneNode(true);
-
-  const input = newFieldContainer.children[0];
-
-  if (input.value == "") {
-    return;
-  }
-
-  input.value = "";
-
-  container.appendChild(newFieldContainer);
+// add photo
+function addPhotoField () {
+    //pegar o container de fotos #images
+    const container = document.querySelector('#images');
+    //pegar o container para duplicar .new-images
+    const fieldsContainer = document.querySelectorAll('.new-upload');
+    //realizar o clone da última image adicionada
+    const newFieldContainer = fieldsContainer[fieldsContainer.length - 1].cloneNode(true);
+    //checar se esta vazio
+    const input = newFieldContainer.children[0];
+    if (input.value == "") {
+        return
+    }
+    //limpar
+    input.value = "";
+    //adicionar o clone ao container de #images
+    container.appendChild(newFieldContainer);
 }
 
-function deleteField(event) {
-  const span = event.currentTarget;
+function deleteField (event) {
+    const span = event.currentTarget;
 
-  const fieldsContainer = document.querySelectorAll(".new-upload");
+    const fieldsContainer = document.querySelectorAll('.new-upload');
 
-  if (fieldsContainer.length <= 1) {
-    span.parentNode.children[0].value = "";
-    return;
-  }
-
-  span.parentNode.remove();
+    if (fieldsContainer.length <= 1) {
+        //limpar o valor do campo
+        span.parentNode.children[0].value = "";
+        return
+    }
+    //deletar
+    span.parentNode.remove();
 }
 
-function toggleSelect(event) {
-  document
-    .querySelectorAll(".button-select button")
-    .forEach((button) => button.classList.remove("active"));
-
-  const button = event.currentTarget;
-  button.classList.add("active");
-
-  const input = document.querySelector('[name="open_on_weekends"]');
-  input.value = button.dataset.value;
+//sim e não
+function toggleSelect (event) {
+    //remove active class from buttons
+    document.querySelectorAll('.button-select button')
+    .forEach(button => button.classList.remove('active'));
+    //update active class
+    const button = event.currentTarget;
+    button.classList.add('active');
+    //update hidden
+    const input = document.querySelector('[name="open_on_weekends"]');
+    //check yes or no
+    input.value = button.dataset.value;
 }
 
-function validade(event) {
-  event.preventDefault()
+function validate(event) {
+    //validate lat and lng
+    const needsLatAndLng = false;
+    if (needsLatAndLng) {
+        event.preventDefault();
+        alert('Selecione um ponto no mapa');
+    }
 }

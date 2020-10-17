83541385b9ac49d742bee99928e4a02e1e8e367b
Paloma V <62178655+palomavila@users.noreply.github.com>
2020-10-17 18:53:15 -0300

    Update test.js

--- src/database/test.js
+++ src/database/test.js
@@ -5,9 +5,8 @@ Database.then(async (db) => {
   await saveOrphanage(db, {
     lat: "-27.2114974",
     lng: "-49.6544793",
-    name: "Lar dos meninos",
-    about:
-      "Presta assistência a crianças de 06 a 15 anos que se encontre em situação de risco e/ou vulnerabilidade social",
+    name: "Boy's home",
+    about:"Presta assistência a crianças de 06 a 15 anos que se encontre em situação de risco e/ou vulnerabilidade social",
     whatsapp: "+55(11)98888-8888",
     images: [
       "https://images.unsplash.com/photo-1583608205776-bfd35f0d9f83?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
